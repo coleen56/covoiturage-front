11e6725f7c6886c0a035998bb3ef1b3137942107
CONTE Coleen <coleen.conte@orange.fr>
2026-03-21 16:28:33 +0100

    adjustments on message page

--- app/(private)/message/components/MessageBodyInput.tsx
+++ app/(private)/message/components/MessageBodyInput.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {inputDivStyle, inputLabelStyle, inputNoHeightStyle} from "@/lib/styles";
 
 interface MessageBodyInputProps {
     label: string;
@@ -12,13 +13,13 @@ interface MessageBodyInputProps {
 export default function MessageBodyInput(props: Readonly<MessageBodyInputProps>) {
     return (
         <div className={"mt-3"}>
-            <label htmlFor={props.name} className="block text-sm/6 font-medium text-black">{props.label}</label>
+            <label htmlFor={props.name} className={inputLabelStyle}>{props.label}</label>
             <div className="">
                 <div
-                    className="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500 border">
+                    className={inputDivStyle}>
                         <textarea id={props.id} name={props.name} placeholder={props.placeholder} value={props.value}
                                   onChange={props.onChange} rows={props.rows}
-                                  className="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-black placeholder:text-gray-500 focus:outline-none sm:text-sm/6"/>
+                                  className={inputNoHeightStyle}/>
                 </div>
             </div>
         </div>
